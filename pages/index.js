12595ce4e01d34684a1e50ee16f026b0ea10519b
Stephan Denis <stephangdenis@gmail.com>
2022-02-10 22:00:02 -0300

    MediumCard + scrollbar-hide plugin

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div>
       <Head>
@@ -31,6 +32,16 @@ export default function Home({ exploreData }) {
             ))}
           </div>
         </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
       </main>
     </div>
   );
@@ -41,9 +52,14 @@ export async function getStaticProps() {
     res.json()
   );
 
+  const cardsData = await fetch("https://jsonkeeper.com/b/PN93").then((res) =>
+    res.json()
+  );
+
   return {
     props: {
       exploreData,
+      cardsData,
     },
   };
 }
