e8bccff6605879eb93325242a8a15a85e4747a1d
Stephan Denis <stephangdenis@gmail.com>
2022-02-10 21:33:18 -0300

    SmallCard

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["upload.wikimedia.org", "a0.muscache.com"],
+    domains: ["upload.wikimedia.org", "a0.muscache.com", "links.papareact.com"],
   },
 };
